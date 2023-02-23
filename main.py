6d1782a27f496a72640deed1de233869c71a7d77
yss <waku.pdxppxzy@gmail.com>
2023-02-23 20:53:33 +0900

    feat: input form sample

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import flet as ft
 
 
-def main(page: ft.Page):
+def counter_app(page: ft.Page):
     page.title = "Flet counter example"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
-    txt_number = ft.TextField(value="0", text_align="right", width=100)
+    txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
 
     def minus_click(e):
         txt_number.value = str(int(txt_number.value) - 1)
@@ -19,12 +19,27 @@ def main(page: ft.Page):
         ft.Row(
             [
                 ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
-                    txt_number,
-                    ft.IconButton(ft.icons.ADD, on_click=plus_click),
+                txt_number,
+                ft.IconButton(ft.icons.ADD, on_click=plus_click),
             ],
             alignment=ft.MainAxisAlignment.CENTER,
         )
     )
 
-ft.app(target=main)
 
+def text_box(page: ft.Page):
+    def btn_click(e):
+        if not txt_name.value:
+            txt_name.error_text = "Please enter your name"
+            page.update()
+        else:
+            name = txt_name.value
+            page.clean()
+            page.add(ft.Text(f"Hello, {name}!"))
+
+    txt_name = ft.TextField(label="Your name")
+    page.add(txt_name, ft.ElevatedButton("Say hello!", on_click=btn_click))
+
+
+# ft.app(target=counter_app)
+ft.app(target=text_box)
