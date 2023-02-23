bef8f0e8e8f5b3e17e4c31a791af6fb1d2bc76a8
yss <waku.pdxppxzy@gmail.com>
2023-02-23 21:17:43 +0900

    feat: add detect keyboard shortcuts sample

---
+++ keyboard_shortcuts.py
@@ -0,0 +1,13 @@
+import flet as ft
+
+
+def keyboard_shortcuts(page: ft.Page):
+    def on_keyboard(e: ft.KeyboardEvent):
+        page.add(
+            ft.Text(
+                f"Key: {e.key}, Shift: {e.shift}, Control: {e.ctrl}, Alt: {e.alt}, Meta: {e.meta}"
+            )
+        )
+
+    page.on_keyboard_event = on_keyboard
+    page.add(ft.Text("Press any key with a combination of CTRL, ALT, SHIFT and META keys..."))
